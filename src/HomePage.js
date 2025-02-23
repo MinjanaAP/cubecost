221dedf190616a808ac3cb0adacefa10c3662278
pasan <pasanathuluwage28@gmail.com>
2025-02-23 22:12:29 +0530

    Edit css parts for mobile response.

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,18 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import './index.css'
 
 const App = () =>{
 
     return (
-        <div
-        style={{
-            backgroundImage: 'url("./images/background-home.png")', // Image URL
-            backgroundSize: "cover", // Cover the entire page
-            backgroundPosition: "center", // Center the background image
-            backgroundRepeat: "no-repeat", // Prevent the image from repeating
-            minHeight: "100vh", // Ensure it takes the full viewport height
-          }}
+        <div className="main-dev"
         >
             <Header />
             <HeroSection />
